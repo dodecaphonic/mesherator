f11a5d1083d5e547dfb4d9ae7ae7fa152e1ad818
Vitor Capela <dodecaphonic@gmail.com>
2013-03-02 14:51:04 -0300

    Removes unused datasets.

--- mesherator.rb
+++ mesherator.rb
@@ -7,59 +7,8 @@ if Rack::Utils.respond_to?("key_space_limit=")
   Rack::Utils.key_space_limit = 262144
 end
 
-POINTS = {
-  small: [],
-  medium: [],
-  large: []
-}
-
-%w(small medium large).each do |data_set|
-  points = open(File.expand_path("../public/data/#{data_set}.txt", __FILE__)).readlines.map { |l|
-    Mesherator::Vector3.new(*l.scan(/\d+,\d+/).map { |v|
-                              v.sub(',', '.').to_f
-                            })
-  }
-
-  POINTS[data_set.to_sym] = points
-end
-
 get '/' do
   send_file File.expand_path('index.html', settings.public_folder)
-end
-
-get '/small/points' do
-  content_type :json
-  POINTS[:small].map { |p| vector3_to_hash(p) }.to_json
-end
-
-get '/small/triangulate' do
-  content_type :json
-  triangulate(POINTS[:small])
-end
-
-get '/small/triangulate/ffi' do
-  content_type :json
-  triangulate_ffi(POINTS[:small])
-end
-
-get '/medium/points' do
-  content_type :json
-  POINTS[:medium].map { |p| vector3_to_hash(p) }.to_json
-end
-
-get '/medium/triangulate' do
-  content_type :json
-  triangulate(POINTS[:medium])
-end
-
-get '/medium/triangulate/ffi' do
-  content_type :json
-  triangulate_ffi(POINTS[:small])
-end
-
-get '/large/points' do
-  content_type :json
-  POINTS[:large].map { |p| vector3_to_hash(p) }.to_json
 end
 
 post '/triangulate/:fast_or_slow' do
@@ -70,16 +19,6 @@ post '/triangulate/:fast_or_slow' do
   else
     triangulate_ffi(json_to_vector3_list(params[:points]))
   end
-end
-
-get '/large/triangulate' do
-  content_type :json
-  triangulate(POINTS[:large])
-end
-
-get '/large/triangulate' do
-  content_type :json
-  triangulate_ffi(POINTS[:large])
 end
 
 def vector3_to_hash(vector3)
